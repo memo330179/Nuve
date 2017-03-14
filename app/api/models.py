464f07c3b461d29605c6b3e3461842c0617f03de
Guillermo Ramos-Macias <guillermoramos330179@gmail.com>
2017-03-14 14:47:39 -0400

    fix errors to inherit properly

--- app/api/models.py
+++ app/api/models.py
@@ -1,15 +1,17 @@
 from flask.ext.sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import SQLAlchemyError
 from marshmallow import Schema, fields, validate
+from slugify import slugify
 
 db = SQLAlchemy()
 
 # Relationships
 
 class Series(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String(250))
     overview = db.Column(db.String(250), nullable=True)
-    series_art = (db.String(250), nullable=True)
+    series_art = db.Column(db.String(250), nullable=True)
 
     def __init__(self, title, overview, series_art):
         self.title = title
@@ -18,7 +20,7 @@ class Series(db.Model):
 
 
 class SerieSchema(Schema):
-
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     not_blank = validate.Length(min=1, error='Field cannot be blank')
     title = fields.String(validate=not_blank)
     overview = db.Column(db.TEXT())
@@ -30,25 +32,46 @@ class SerieSchema(Schema):
         fields = ('title', 'overview', 'series_art')
 
 class Tags(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     tag = db.Column(db.String(250))
 
-class Season(db.model):
+class Season(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     number = db.Column(db.Integer)
     overview = db.Column(db.TEXT())
     art = db.Column(db.String())
 
 class Media_File(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String(250))
     path = db.Column(db.String(250))
     slug = db.Column(db.String(250))
     image_path = db.Column(db.String(250))
     overview = db.Column(db.TEXT())
+    media_type = db.Column(db.String(250))
+
+    __mapper_args__ = {'polymorphic_on': media_type}
+
+    def __init__(self, title, path, image_path, overview):
+        self.title = title
+        self.path = path
+        self.slug = self.create_slug(title)
+        self.image_path = image_path
+        self.overview = overview
+
+    def create_slug(self, title):
+        return slugify(title)
+
 
 class Episode(Media_File):
-    Season = db.Column(db.Integer, db.ForeignKey(Season))
+    __table_args__ = {'extend_existing': True}
+    __mapper_args__ = {'polymorphic_identity': 'Episode'}
+    Season = db.Column(db.Integer, db.ForeignKey(Season.id))
 
 
 class Movie(Media_File):
+    __table_args__ = {'extend_existing': True}
+    __mapper_args__ = {'polymorphic_identity': 'Movie'}
     release_date = db.Column(db.Date)
     runtime = db.Column(db.Integer)
 
@@ -61,6 +84,3 @@ def session_commit():
         db.session.rollback()
         reason = str(e)
         return reason
-
-if __name__ == "__main__":
-    db.create_all()
