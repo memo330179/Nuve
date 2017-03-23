6c73f05526a611c556ef1338f0ce4c1875cc38ff
Guillermo Ramos-Macias <guillermoramos330179@gmail.com>
2017-03-23 16:03:49 -0400

    guess info about media file

--- app/api/views.py
+++ app/api/views.py
@@ -2,22 +2,27 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for,
 from flask_restful import Resource, Api
 import flask_restful
 from app.mod_auth.views import login_required
+from guessit import guessit
 
 media_api = Blueprint('media_api', __name__)
 
 
 # we need to upload a file
 # this is the simplest way I could do it
-@media_api.route("/upload/<media_type>", methods=["POST"])
+@media_api.route("/upload/media", methods=["POST"])
 def upload_media(media_type):
-    media_file = request.files['file']
-    if media_type = "movie":
-        #upload_movie(media_file)
-        pass
-    else if media_type = "tv":
-        #upload_tv(media_file)
-        pass
+    media_files = request.files['file[]']
 
+    for media_file in media_files:
+
+        filename = secure_filename(media_file.filename)
+
+        guessed_info = guessit(filename)
+
+        if guessed_info.type == "Movie":
+            upload_movie(media_file)
+        elif guessed_info.type == "Episode":
+            upload_tv(media_file, guessed_info)
 
 
 rest_api = Api(media_api)
